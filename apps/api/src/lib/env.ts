59cd192b7104f6c8e2865f782c6adc7de7e1d0cf
Adair Juneo <adair_juneo@hotmail.com>
2025-01-14 22:34:29 -0300

    creating a env package for sharing environment variables with all monorepo.

--- apps/api/src/lib/env.ts
+++ apps/api/src/lib/env.ts
@@ -1,32 +1,3 @@
-import chalk from 'chalk';
-import { z } from 'zod';
+import { env as envSchema } from '@saas-rbac/env';
 
-const envSchema = z.object({
-  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().min(1),
-  JWT_SECRET: z.string().min(1),
-  RESEND_API_KEY: z.string().min(1),
-  PAGINATION_PAGE_SIZE: z.coerce.number().max(20),
-  AUTH_SALT_PASSWORD_HASH: z.coerce.number().max(6),
-  AUTH_KEY_EXPIRATION_SECONDS: z.coerce.number().min(120),
-  GITHUB_CLIENT_ID: z.string().min(1),
-  GITHUB_CLIENT_SECRET: z.string().min(1),
-  // GITHUB_AUTHORIZE_URL: z.string().url(),
-  GITHUB_REDIRECT_URL: z.string().url(),
-  GITHUB_USER_DATA_URL: z.string().url(),
-  GITHUB_ACCESS_TOKEN_URL: z.string().url(),
-});
-
-const _env = envSchema.safeParse(process.env);
-
-if (_env.success === false) {
-  console.error(
-    chalk.bgRedBright('❌ Invalid environment variables.'),
-    _env.error.format()
-  );
-
-  throw new Error('Invalid environment variables.');
-}
-
-export const env = _env.data;
+export const env = envSchema;
---
+++ packages/env/src/index.ts
@@ -0,0 +1,41 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.string().min(1),
+    JWT_SECRET: z.string().min(1),
+    RESEND_API_KEY: z.string().min(1),
+    PAGINATION_PAGE_SIZE: z.coerce.number().max(20),
+    AUTH_SALT_PASSWORD_HASH: z.coerce.number().max(6),
+    AUTH_KEY_EXPIRATION_SECONDS: z.coerce.number().min(120),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
+    GITHUB_REDIRECT_URL: z.string().url(),
+    GITHUB_USER_DATA_URL: z.string().url(),
+    GITHUB_ACCESS_TOKEN_URL: z.string().url(),
+    // DATABASE_URL: z.string().url(),
+    // OPEN_AI_API_KEY: z.string().min(1),
+    // GITHUB_AUTHORIZE_URL: z.string().url(),
+  },
+  client: {},
+  shared: {},
+  emptyStringAsUndefined: true,
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    PAGINATION_PAGE_SIZE: process.env.PAGINATION_PAGE_SIZE,
+    AUTH_SALT_PASSWORD_HASH: process.env.AUTH_SALT_PASSWORD_HASH,
+    AUTH_KEY_EXPIRATION_SECONDS: process.env.AUTH_KEY_EXPIRATION_SECONDS,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GITHUB_REDIRECT_URL: process.env.GITHUB_REDIRECT_URL,
+    GITHUB_USER_DATA_URL: process.env.GITHUB_USER_DATA_URL,
+    GITHUB_ACCESS_TOKEN_URL: process.env.GITHUB_ACCESS_TOKEN_URL,
+  },
+});
