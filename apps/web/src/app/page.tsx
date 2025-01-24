06e7647392ec2570d6012da250a7fedb55110628
Adair Juneo <adair_juneo@hotmail.com>
2025-01-24 17:44:24 -0300

    creating app web setup for develop all interface with next.js

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-full bg-slate-950 text-slate-100">
+      <main className="">
+        <h1 className="text-3xl font-bold">SaaS RBAC Web</h1>
+      </main>
+    </div>
+  );
+}
